35a3bea8834db8ca92bb5957d46e928445491aed
Ariharasudhan <100225369+arihara-sudhan@users.noreply.github.com>
2023-04-22 16:06:43 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Banner from './components/banner'
+import Posts from './components/posts'
+import End from './components/end'
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <Banner />
+      <Posts />
+      <End />
+    </div>
+  );
+}
+
+export default App;
