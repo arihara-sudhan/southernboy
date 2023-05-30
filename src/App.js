c80967f7345748bc30614d818eff3b54c72d6a10
Ariharasudhan <100225369+arihara-sudhan@users.noreply.github.com>
2023-05-30 08:17:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import Banner from './components/banner'
-import Posts from './components/posts'
-import End from './components/end'
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Banner from './components/banner';
+import Posts from './components/posts';
+import End from './components/end';
 import './App.css';
 
 function App() {
+  const [spinner, setSpinner] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setSpinner(false), 1000);
+  }, []);
+
+  if (spinner) {
+    return <center>LOADING</center>;
+  }
+
   return (
-    <div className="app">
-      <Banner />
-      <Posts />
-    </div>
+    <Router>
+      <div className="app">
+        <Banner />
+        <Posts />
+        <End />
+      </div>
+    </Router>
   );
 }
 
