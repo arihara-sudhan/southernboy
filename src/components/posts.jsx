e5fde85ffe0a0a043878b347914f510e9f0dd4a2
Aravind Ariharasudhan <100225369+arihara-sudhan@users.noreply.github.com>
2023-07-24 17:53:50 +0530

    Update posts.jsx

--- src/components/posts.jsx
+++ src/components/posts.jsx
@@ -9,7 +9,7 @@ export default function Posts() {
   const { id } = useParams();
   const TOTALTOPS = 5;
   const [posts, setPosts] = useState([]);
-  const [which, setWhich] = useState(0);
+  const [which, setWhich] = useState(parseInt(id, 10) || 0); // Parse the id to an integer or default to 0 if id is not provided
   const [set, setSet] = useState([0, TOTALTOPS - 1]);
   const [showPosts, setShowPosts] = useState(false);
   const [loading, setLoading] = useState(true);
@@ -22,6 +22,7 @@ export default function Posts() {
     });
   }, []);
 
+  // Function to navigate to a specific post
   const navigateToPost = useCallback(
     (index) => {
       setWhich(index);
@@ -31,31 +32,36 @@ export default function Posts() {
     [navigate]
   );
 
+  // Function to navigate to the previous set of posts
   const navigateToPrevSet = useCallback(() => {
     if (set[0] - TOTALTOPS >= 0 && set[0] - 1 >= 0) {
       setSet([set[0] - TOTALTOPS, set[0] - 1]);
     }
   }, [set]);
 
+  // Function to navigate to the next set of posts
   const navigateToNextSet = useCallback(() => {
     if (set[1] + 1 < posts.length) {
       setSet([set[0] + 1, set[1] + 1]);
     }
   }, [set, posts.length]);
-  
 
+  // Function to toggle showing all posts
   const toggleShowPosts = useCallback(() => {
     setShowPosts((prevState) => !prevState);
   }, []);
 
+  // Prevent drag event handler
   const preventDragHandler = (event) => {
     event.preventDefault();
   };
 
+  // Prevent context menu event handler
   const preventContextMenuHandler = (event) => {
     event.preventDefault();
   };
 
+  // Function to render a single post
   const renderPost = () => {
     if (posts.length === 0) return null;
 
@@ -77,6 +83,7 @@ export default function Posts() {
     );
   };
 
+  // Function to render all posts
   const renderAllPosts = () => {
     return (
       <div className='allposts'>
@@ -89,6 +96,7 @@ export default function Posts() {
     );
   };
 
+  // Function to render logos for posts
   const renderLogos = () => {
     const logos = posts.map((post) => (
       <img
